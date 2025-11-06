fa52a8a909d5e1ea758b651858591b716608d476
pmoguel <pmoguel@centro.edu.mx>
2025-11-06 15:01:01 -0600

    Trabajo en clase: Animacion de entrada y salida con mouse

--- S12/E1-Image-Trail/B2-Images-Trail-main/main.js
+++ S12/E1-Image-Trail/B2-Images-Trail-main/main.js
@@ -1,5 +1,41 @@
 console.log('main.js');
 
+/*
+1. Definir en una constante el espacio mínimo que deseamos entre cada imagen.
+2. Crear una variable tipo objeto para guardar la posición de la última imagen creada y poder medir la distancia al mouse.
+3. En el evento “mousemove” donde estamos invocando la función para crear imágenes, vamos a definir la condición para crear la imagen:
+    a. Primero calculamos la distancia en X y Y entre la posición de la última imagen y el mouse actualmente.
+    b. Si la distancia calculada es mayor a nuestro espacio mínimo, solo entonces creamos la imagen.
+    c. Finalmente, actualizamos el registro de la posición de la última imagen ya que creamos una nueva.
+
+*/
+const minDistance = 50;
+var lastPos = {
+    x: 0,
+    y: 0,
+}
+
+
+// 04. Mostrarlas en ciclo.
+/*
+    1.Crear una lista con la ubicación de las imágenes a renderizar.
+    2.Actualizar nuestra función para que lea un valor de esta lista para renderizar nuestra imagen.
+    3.Definir una variable “índice” que nos indique qué imagen debe renderizar nuestra función.
+    4.Avanzar 1 posición el valor del índice cada vez que creamos una imagen.
+    5.Usar este índice para definir qué valor en la lista queremos leer.
+    6.Reiniciar el índice al alcanzar el máximo.
+*/
+var imageList= [
+    "./assets/p1.jpg",
+    "./assets/p2.jpg",
+    "./assets/p3.jpg",
+    "./assets/p4.jpg",
+    "./assets/p5.jpg",
+    "./assets/p6.jpg"
+]
+
+var index = 0;
+
 // 01. Renderizar 1 imagen.
 /*
 1. Crear etiqueta <img> virtual.
@@ -13,20 +49,69 @@ console.log('main.js');
 5. Agrupar estas instrucciones en una función.
 */
 
-const img = document.createElement("img");
-img.src = "./assets/p1.jpg";
-img.style.width = "227px";
-img.style.height = "150px";
-img.style.top = "50px";
-img.style.left = "50px";
-img.style.position = "absolute";
-document.body.appendChild(img);
+function createFloatingImage(posX, posY){
+    console.log(posX, posY);    
+    const img = document.createElement("img");
+    img.src = imageList[index];
+    img.style.width = "227px";
+    img.style.height = "150px";
+    img.style.top = `${posY - 75}px`;
+    img.style.left = posX - (113.5) + "px";
+    img.style.position = "absolute";
+    img.style.opacity = 0;
+    document.body.appendChild(img);
+
+    gsap.to(img,{
+        opacity: 1,
+        duration: 0.7,
+        ease: "power3.out"
+    });
+
+    index = index + 1;
+    if(index >= imageList.length){
+        index = 0;
+    }
+
+
+    setTimeout(function() {
+        gsap.to(img,{
+            opacity: 0,
+            duration: 0.7,
+            ease: "power3.out",
+            onComplete: function() {
+                img.remove();
+            }
+        });
+
+    }, 700);
+}
+
+
 
 // 02. Renderizar “n” imágenes.
+
+window.addEventListener("mousemove", function (eventData){
+    console.log(eventData);
+    // calcular la distancia entre el mouse y la ultima imagen dibujada para evaluar nuestra condicion para renderizar o no.
+    //1. Calcular la distancia
+    var dx = eventData.clientX - lastPos.x;
+    dx = Math.abs(dx);
+    var dy = eventData.clientY - lastPos.y;
+    dy = Math.abs(dy);
+
+    if (dx >= minDistance || dy >= minDistance){
+        createFloatingImage(eventData.clientX, eventData.clientY);
+        lastPos.x = eventData.clientX;
+        lastPos.y = eventData.clientY;
+    }
+});
+
 // 03. Posicionarlas según el mouse.
-// 04. Mostrarlas en ciclo.
+
 // 05. Desaparecerlas después de “x” tiempo.
 // 06. Hacer su animación de salida.
+
+
 // 07. Hacer su animación de entrada.
 // 08. Renderizarlas cada “x” distancia.
 // 09. Renderizarlas adelante y atrás de cada letra.
